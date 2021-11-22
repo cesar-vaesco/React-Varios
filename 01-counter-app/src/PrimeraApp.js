b6c37c38435f820a976efddeec1e859981fefc0e
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-21 21:42:06 -0600

    Counter-App: Fragment /Retornando elementos en el componente

--- 01-counter-app/src/PrimeraApp.js
+++ 01-counter-app/src/PrimeraApp.js
@@ -3,7 +3,13 @@ import React from 'react';
 
 const PrimeraApp = () => {
 
-    return <h1>Hola Mundo</h1>
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primer Aplicación</p>
+        </>
+    );
+
 }
 
 
