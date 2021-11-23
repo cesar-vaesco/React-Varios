b73fe3e5e3140a1926996590d855ae4be50c2f5c
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-22 21:15:01 -0600

    CounterApp: Evento click - introducción a los eventos

--- 01-counter-app/src/CounterApp.js
+++ 01-counter-app/src/CounterApp.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+    //HandleAdd
+    const handleAdd = (e) => {
+        console.log(e);
+
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> {value} </h2>
+
+            <button onClick={
+                handleAdd
+            }>+1</button>
 
         </>
     );
