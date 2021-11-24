94a679c8534119ce693f6e5c4b31b234307f7085
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-23 19:57:45 -0600

    Test-CounterApp:  ejemplo de estructura / conociendo los métodos expect-toBe

--- 01-counter-app/src/tests/demo.test.js
+++ 01-counter-app/src/tests/demo.test.js
@@ -1,9 +1,17 @@
 
-test('debe de ser true', () => {
+describe('Pruebas en el archivo demo.test.js', () => {
 
-    const isActive = true;
+    test('deben de ser iguales los strings', () => {
 
-    if(isActive) {
-        throw new Error('No esta activo');
-    }
-})
+        // 1.- Inicialización
+        const mensaje = 'Hola mundo';
+
+        //  2.- estimulo
+        const mensaje2 = 'Hola mundo';
+
+        // 3.- Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+
+    })
+
+});
