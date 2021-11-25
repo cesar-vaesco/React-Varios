6f3614feeb7c88ada81e2deaedc8489cee76dbe2
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-24 21:40:52 -0600

    Test-CounterApp: Instalando y usando la librería Enzyne - Testing unit

--- 01-counter-app/src/tests/PrimeraApp.test.js
+++ 01-counter-app/src/tests/PrimeraApp.test.js
@@ -1,18 +1,25 @@
 import '@testing-library/jest-dom';
 
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
-
-import { render } from '@testing-library/react';
+// eslint-disable-next-line no-unused-vars
 import PrimeraApp from '../PrimeraApp';
+
+import { shallow } from 'enzyme';
 
 describe('Pruebas en PrimeraApp', () => {
 
-    test('Debe de mostrar el mensaje: Hola, Soy Goku', () => {
-        // 1.- Inicialización
+    // test('Debe de mostrar el mensaje: Hola, Soy Goku', () => {
+    //     const saludo = 'Hola, Soy Goku';
+    //     const { getByText } = render(<PrimeraApp saludo={saludo} />);
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // });
+
+    test('Debe de mostrer <PrimeraApp/> correctamente', () => {
+
         const saludo = 'Hola, Soy Goku';
+        const wrapper = shallow(<PrimeraApp saludo={saludo} />);
 
-        const { getByText } = render(<PrimeraApp saludo={saludo} />);
-
-        expect(getByText(saludo)).toBeInTheDocument();
+        expect( wrapper).toMatchSnapshot();
     });
 });
