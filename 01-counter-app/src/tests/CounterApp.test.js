2363c3cab0e9d62929fa9835d8a2ac1f69aed60f
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-28 14:45:11 -0600

    Test-CounterApp: Simular eventos - Click

--- 01-counter-app/src/tests/CounterApp.test.js
+++ 01-counter-app/src/tests/CounterApp.test.js
@@ -6,9 +6,16 @@ import CounterApp from '../CounterApp';
 
 describe('Pruebas en <CounterApp/>', () => {
 
+    let wrapper = shallow(<CounterApp />);
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp />);
+
+    })
+
     test('Debe mostrar <CounterApp/> correctamente', () => {
 
-        const wrapper = shallow(<CounterApp />);
+
         // 3.- Observar el comportamiento
         expect(wrapper).toMatchSnapshot();
     });
@@ -23,5 +30,26 @@ describe('Pruebas en <CounterApp/>', () => {
         expect(counterText).toBe('100');
 
         //expect(wrapper.find('h2').text().trim()).toBe('100');
+    });
+
+    test('Debe de incrementar en +1', () => {
+        //const wrapper = shallow(<CounterApp />);
+        //const btn1 = wrapper.find('button').at(0);
+        // console.log(btn1.html());
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('11');
+    });
+
+    test('Debe de decrementar en -1', () => {
+        //const wrapper = shallow(<CounterApp />);
+        //const btn1 = wrapper.find('button').at(0);
+        // console.log(btn1.html());
+        // wrapper.find('button').at(2).simulate('click');
+        // const counterText = wrapper.find('h2').text().trim();
+        // expect(counterText).toBe('9');
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('9');
     });
 });
