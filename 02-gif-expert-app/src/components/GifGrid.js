ed720db497bc7052338d8c1e6b1d7c7789ce7490
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-29 11:28:21 -0600

    GifExpertApp:  mostrando titulos e imagenes en la vista

--- 02-gif-expert-app/src/components/GifGrid.js
+++ 02-gif-expert-app/src/components/GifGrid.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
 
-    const [count, setCount] = useState( 0 );
+    const [images, setImages] = useState([]);
 
     useEffect(() => {
         getGifs();
@@ -19,6 +20,8 @@ export const GifGrid = ({ category }) => {
         const { data } = await resp.json();
 
 
+
+
         const gifs = data.map( img => {
             return {
                 id: img.id,
@@ -27,16 +30,33 @@ export const GifGrid = ({ category }) => {
             }
         })
 
+
+
         console.log( gifs );
+        setImages( gifs );
+
+
     };
+
+
+
+
 
     //getGifs();
 
     return (
         <div>
             <h3>{ category }</h3>
-            <p>{ count }</p>
-            <button onClick={ () => setCount( count + 1 )}></button>
+
+                {
+                    images.map( ( img )=>
+                         <GifGridItem
+                            key = { img.id }
+                             {...img }
+                        />
+                    )
+                }
+
         </div>
     )
 }
