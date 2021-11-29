94216fac0666321134ae12e2111ebef861621d73
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-11-29 10:54:56 -0600

    GifExpertApp:  ejemplo del hook useEffect

--- 02-gif-expert-app/src/components/GifGrid.js
+++ 02-gif-expert-app/src/components/GifGrid.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({ category }) => {
+
+
+    const [count, setCount] = useState( 0 );
+
+    useEffect(() => {
+        getGifs();
+    }, []);
 
 
     const getGifs = async () => {
 
         const url = 'https://api.giphy.com/v1/gifs/search?q=One+Punch&limit=10&api_key=7lB878Nhu3ZkNDikahV5dr03vHbNXJkU';
 
-        const resp = await fetch(url);
+        const resp = await fetch( url );
 
         const { data } = await resp.json();
 
 
-        const gifs = data.map(img => {
+        const gifs = data.map( img => {
             return {
                 id: img.id,
                 title: img.title,
@@ -20,14 +27,16 @@ export const GifGrid = ({ category }) => {
             }
         })
 
-        console.log(gifs);
+        console.log( gifs );
     };
 
-    getGifs();
+    //getGifs();
 
     return (
         <div>
-            <h3>{category}</h3>
+            <h3>{ category }</h3>
+            <p>{ count }</p>
+            <button onClick={ () => setCount( count + 1 )}></button>
         </div>
     )
 }
